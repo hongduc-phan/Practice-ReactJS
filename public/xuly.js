3af0e84639e221b6801b97e2db356e41f03ce167
Duc Phan <phduc.phanhongduc@gmail.com>
2017-11-01 05:37:33 +0200

    divide ReactJs outside HTML file

---
+++ public/xuly.js
@@ -0,0 +1,8 @@
+ReactDOM.render
+( <h1 className="yellow">Hello</h1>, 
+            document.getElementById("root"));
+
+ReactDOM.render
+            ( <h1 className="yellow">PHAN</h1>, 
+                        document.getElementById("root2"));
+
