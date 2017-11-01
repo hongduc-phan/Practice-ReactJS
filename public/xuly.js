8c6901053bcbde9cee899244003d64bde75df4d9
Duc Phan <phduc.phanhongduc@gmail.com>
2017-11-01 06:35:49 +0200

    demo component is nested other component

--- public/xuly.js
+++ public/xuly.js
@@ -1,7 +1,19 @@
 class KhoaPham extends React.Component{
     render(){
         return (
+            <div>
                     <h1 className = "yellow"> Khoa Pham React </h1>
+                    <KhoaHoc/>
+                    </div>
+                );
+            }
+    
+};
+
+class KhoaHoc extends React.Component{
+    render(){
+        return (
+                    <h1 className = "red"> Khoa Pham React </h1>
                 );
             }
     
